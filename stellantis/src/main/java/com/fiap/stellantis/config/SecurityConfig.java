92d47497cd7e7f818282517cf5d4de51a0920871
IsabelaMarques07 <dev.isabelamarques@gmail.com>
2022-11-11 18:27:18 -0300

    sistema de autenticação

---
+++ stellantis/src/main/java/com/fiap/stellantis/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.fiap.stellantis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("usuario").password(passwordEncoder().encode("senha"))
+		.roles("usuario");
+	}
+	
+	
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.authorizeRequests()
+		.anyRequest()
+		.authenticated()
+		.and()
+		.httpBasic()
+		.and()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+}
---
+++ stellantis/src/main/java/com/fiap/stellantis/controllers/LoginController.java
@@ -0,0 +1,20 @@
+package com.fiap.stellantis.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+	
+	
+	@GetMapping("/entrar")
+	@PreAuthorize("hasRole('usuario')")
+	public ModelAndView getOpen() {
+		return new ModelAndView("redirect:/home");
+	}
+}
